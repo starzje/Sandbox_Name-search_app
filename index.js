03288142053225868fd04849d07d897ffc964f61
Ivan Starčević <istarcevic16@gmail.com>
2022-06-23 12:45:15 +0200

    Updated button functionality

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const prethodnePretrageText = document.getElementById("prethodnePretrage");
 const rezultatiPretrageText = document.getElementById("div1");
 
+//prebacuje oznake regija u puni naziv na hrvatskom HR -> Hrvatska
+const regionNamesInCroatian = new Intl.DisplayNames(["hr"], { type: "region" });
+
 document
   .getElementById("botunZaSlanje")
   .addEventListener("click", dohvatiPodatke);
 
 function dohvatiPodatke(event) {
+  event.preventDefault();
   let unos = event.target.previousElementSibling.value;
   fetch(`https://api.nationalize.io/?name=${unos}`)
     .then((response) => response.json())
@@ -17,15 +21,10 @@ function dohvatiPodatke(event) {
 
 //funkcija koja ispisuje rezultate na stranicu
 function prikaziNoveRezultate(data) {
-  //prebacuje oznake regija u puni naziv na hrvatskom HR -> Hrvatska
-  const regionNamesInCroatian = new Intl.DisplayNames(["hr"], {
-    type: "region",
-  });
-
   // ako ime ne postoji ulazi se u prvi IF
   if (data.country[0] === undefined) {
     rezultatiPretrageText.innerHTML = `${data.name} nije ime koje postoji. Pokušaj ponovno.`;
-    prethodnePretrageText.innerHTML += `<button type="button" class="btn btn-light">${data.name}</button> `;
+    prethodnePretrageText.innerHTML += `<button type="button" class="btn btn-light undefined_btn">${data.name}</button> `;
   }
   //ako ime postoji tj. sadržava objekt country ulazi se u else
   else {
@@ -69,7 +68,32 @@ function prikaziNoveRezultate(data) {
 </ul>`;
     }
 
-    // ispisuje prethodno pretrazivano ime
-    prethodnePretrageText.innerHTML += `<button type="button" class="btn btn-light">${data.name}</button> `;
+    let svePretrage = [data.name + " = "];
+    for (let i = 0; i < data.country.length; i++) {
+      svePretrage.push(
+        regionNamesInCroatian.of(data.country[i].country_id) +
+          " " +
+          Math.ceil(data.country[i].probability * 100) +
+          "%"
+      );
+    }
+
+    prethodnePretrageText.innerHTML += `<button type="button" class="btn btn-light">${svePretrage.join(
+      " "
+    )}</button> `;
+
+    //ispisuje sve pretrage u prethodnePretrageText
+
+    prethodnePretrageText.addEventListener("click", function (event) {
+      let unos = event.target.innerText;
+      let niz = unos.split("=");
+      let ime = niz[0];
+      let zemlje = niz.slice(1);
+      if (event.target.classList.contains("undefined_btn")) {
+        rezultatiPretrageText.innerHTML = `Već si provjerio ove podatke, i znaš da ${ime} nije pravo ime`;
+      } else {
+        rezultatiPretrageText.innerHTML = `Već si provjerio ove podatke, ali vjerojatnost da je ${ime} iz zemlje ${zemlje}`;
+      }
+    });
   }
 }
